e7ece87d91fec9c54f565465e6ebd67da394df25
Elvis Camacho <45659660+ElvisCamacho@users.noreply.github.com>
2023-05-28 21:00:23 +0200

    Update hello.py

--- myapp/hello.py
+++ myapp/hello.py
@@ -1,7 +1,29 @@
 import fire
 
-def hello(name="World"):
-  return "Hello %s!" % name
+class Calculator:
+    def __init__(self):
+        self.result = 0
+
+    def add(self, x, y):
+        """Add two numbers"""
+        self.result = x + y
+        return self.result
+
+    def divide(self, x, y):
+        """Divide two numbers"""
+        if y == 0:
+            print("Error: Division by zero!")
+        else:
+            self.result = x / y
+            return self.result
 
 if __name__ == '__main__':
-  fire.Fire(hello)
+    fire.Fire(Calculator)
+
+#import fire
+
+#def hello(name="World"):
+#  return "Hello %s!" % name
+#
+#if __name__ == '__main__':
+#  fire.Fire(hello)
