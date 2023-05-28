adda7977096c96779248c43e11a44cbebb2ddd8c
Elvis <camachocv@hotmail.com>
2023-05-28 21:05:25 +0200

    M

--- myapp/hello.py
+++ myapp/hello.py
@@ -1,29 +1,7 @@
 import fire
 
-class Calculator:
-    def __init__(self):
-        self.result = 0
-
-    def add(self, x, y):
-        """Add two numbers"""
-        self.result = x + y
-        return self.result
-
-    def divide(self, x, y):
-        """Divide two numbers"""
-        if y == 0:
-            print("Error: Division by zero!")
-        else:
-            self.result = x / y
-            return self.result
+def hello(name="World"):
+  return "Hello %s!" % name
 
 if __name__ == '__main__':
-    fire.Fire(Calculator)
-
-#import fire
-
-#def hello(name="World"):
-#  return "Hello %s!" % name
-#
-#if __name__ == '__main__':
-#  fire.Fire(hello)
+  fire.Fire(hello)
